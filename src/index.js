f3002a1f9c063e691f25053af4855b323ced78c6
yrusak <yauheni.rusak@godeltech.com>
2020-03-20 18:02:40 +0300

    feat: complete solution

--- src/index.js
+++ src/index.js
@@ -4,52 +4,64 @@ module.exports = function check(str, bracketsConfig) {
     if (checkBracketsPaired(str, bracketsConfig)) {
         for (let i = 0; i < bracketsConfig.length; i++) {
             let result = checkComplex(str, bracketsConfig, i);
-            console.log('end', result);
             res &= result;
         }
+    } else {
+        return false;
     }
     return res;
 
     function checkComplex(ss, pattern, i) {
+        let result = true;
         let lastOpen;
         let firstClose;
+
         if (pattern[i][0] == pattern[i][1]) {
             lastOpen = ss.indexOf(pattern[i][0]);
-            console.log('open', lastOpen);
-            firstClose = ss.slice(0, firstClose).lastIndexOf(pattern[i][1]);
-            console.log('close', firstClose);
-        } else {
-            firstClose = ss.indexOf(pattern[i][1]);
-            console.log('close', firstClose);
-            lastOpen = ss.slice(0, firstClose).lastIndexOf(pattern[i][0]);
-            console.log('open', lastOpen);
-        }
-            let block = ss.slice(lastOpen, firstClose + 1);
-            console.log('block', block);
-            let result = true;
-            if (firstClose == -1 && lastOpen == -1) {
-                result = true;
-                console.log('1', result);
-            } else if (firstClose == -1 || lastOpen == -1) {
-                result = false;
-                console.log('2', result);
+            if (lastOpen != -1) {
+                firstClose = lastOpen + 1 + ss.substr(lastOpen + 1).indexOf(pattern[i][1]);
             } else {
-                if (pattern.length > i + 1) {
-                    result &= checkComplex(block, pattern, i + 1);
-                    console.log('3', result)
-                }
-                let replaced = ss.replace(block, '');
-                result &= checkComplex(replaced, pattern, i);
-                console.log('4', result);
+                firstClose = lastOpen;
             }
-
-            return result;
-        }
-
-        function checkBracketsPaired(exp, pattern) {
-            if (!(((exp.match(new RegExp('/[' + pattern[0] + ']/g')) || []).length) == ((exp.match(new RegExp('/[' + pattern[1] + ']/g')) || []).length))) {
+            if (firstClose == lastOpen && lastOpen != -1) {
                 return false;
             }
-            return true;
+        } else {
+            firstClose = ss.indexOf(pattern[i][1]);
+            lastOpen = ss.slice(0, firstClose).lastIndexOf(pattern[i][0]);
         }
+        let block = ss.slice(lastOpen, firstClose + 1);
+        result = true;
+        if (firstClose == -1 && lastOpen == -1) {
+            result &= true;
+        } else if (firstClose == -1 || lastOpen == -1) {
+            return false;
+        } else {
+
+            if (pattern.length > i + 1) {
+                result &= checkComplex(block, pattern, i + 1);
+            }
+            let replaced = ss.replace(block, '');
+            if (replaced != '') {
+                result &= checkComplex(replaced, pattern, i);
+            }
+
+        }
+        return result;
     }
+
+    function checkBracketsPaired(exp, pattern) {
+        let result = true;
+        for (let i = 0; i < pattern.length; i++) {
+            let open = '\[\\' +  pattern[i][0] + '\]';
+            let close = '\[\\' +  pattern[i][1] + '\]';
+
+            if ((((exp.match(new RegExp(open, 'g'))) || []).length) == ((exp.match(new RegExp(close, 'g')) || []).length)) {
+                result &= true;
+            } else {
+                result &= false;
+            }
+        }
+        return result;
+    }
+}
